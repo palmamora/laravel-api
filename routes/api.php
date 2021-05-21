d5d2e08d04e88730fe10af37a68f4fdcdd0c881b
Manuel Eduardo Palma Mora <m.palma12@ufromail.cl>
2021-05-21 17:48:22 -0400

    JWT añadido

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,13 @@ use App\Article;
 |
 */
 
-Route::get('articles', 'ArticleController@index');
-Route::get('articles/{article}', 'ArticleController@show');
-Route::post('articles', 'ArticleController@store');
-Route::put('articles/{article}', 'ArticleController@update');
-Route::delete('articles/{article}', 'ArticleController@delete');
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@authenticate');
+Route::group(['middleware' => ['jwt.verify']], function () {
+    Route::get('articles', 'ArticleController@index');
+    Route::get('user', 'UserController@getAuthenticatedUser');
+    Route::get('articles/{article}', 'ArticleController@show');
+    Route::post('articles', 'ArticleController@store');
+    Route::put('articles/{article}', 'ArticleController@update');
+    Route::delete('articles/{article}', 'ArticleController@delete');
+});
