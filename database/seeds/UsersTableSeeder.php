9e20e6b339c8cbd8f0e426837ea00e9967b37a14
Manuel Eduardo Palma Mora <m.palma12@ufromail.cl>
2021-05-21 16:08:40 -0400

    seeder para User y update del DatabaseSeeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        $password = Hash::make('123123');
+
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@prueba.com',
+            'password' => $password,
+        ]);
+
+        for ($i=0; $i < 6; $i++) { 
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]); 
+        }
+    }
+}
