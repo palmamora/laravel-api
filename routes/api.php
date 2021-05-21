64a8131c1b3ba0c567836492e72af94e37de08ed
Manuel Eduardo Palma Mora <m.palma12@ufromail.cl>
2021-05-21 16:19:12 -0400

    creación rutas del api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,21 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('articles', function () {
+    return Article::all();
+});
+Route::get('articles/{id}', function ($id) {
+    return Article::find($id);
+});
+Route::post('articles', function (Request $request) {
+    return Article::create($request->all());
+});
+Route::put('articles/{id}', function (Request $request, $id) {
+    $article = Article::findOrFail($id);
+    $article->update($request->all());
+    return $article;
+});
+Route::delete('articles/{id}', function ($id) {
+    Article::find($id)->delete();
+    return 204;
 });
